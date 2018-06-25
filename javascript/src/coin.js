34c367a447b8085e410b80c0428f551313e363b6
shihhanwang <swang05@mail.bbk.ac.uk>
2018-06-25 16:43:11 +0100

    07: jus | src | change function added

--- javascript/src/coin.js
+++ javascript/src/coin.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function Coin(DEFAULTCOIN = [50, 20, 10, 5, 2, 1]){
+  this.coins = DEFAULTCOIN;
+};
+
+Coin.prototype.change = function (amount) {
+  const result = []
+  this.coins.forEach(function(c) {
+    if (amount >= c) {
+      var count = Math.floor(amount / c)
+      for(var i = 0; i < count; i++){
+        result.push(c);
+      }
+      amount -= (c * count);
+    }
+  });
+  return result;
+};
